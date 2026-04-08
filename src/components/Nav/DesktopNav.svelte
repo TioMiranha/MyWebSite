3634ffc1b85322ef4e7e0e8047d531d958028f64
Eduardo Silva <eduardosilvajs.py@gmail.com>
2026-04-08 08:06:38 -0400

    Lançando organização de arquitetura

---
+++ src/components/Nav/DesktopNav.svelte
@@ -0,0 +1,34 @@
+<nav class="bg-[#1b263b] lg:block">
+	<div class="mx-auto hidden px-4 sm:px-6 lg:block lg:px-15">
+		<div class=" flex h-16 items-center justify-between">
+			<div class="flex items-center">
+				<a href="" class="shrink-0">
+					<img src="/images/svelte-logo-by-gengns.png" alt="logo bacana" class="h-10 w-10" />
+				</a>
+			</div>
+				<div class="hidden lg:block items-center">
+					<div class="ml-10 flex items-baseline space-x-4 lg:text-xs xl:text-base">
+						<a
+							href=""
+							class="rounded-lg px-3 py-2 font-medium text-[#dce0e6] transition duration-300 ease-in-out hover:bg-[#2b4c7e]"
+							>Testando</a
+						>
+						<a
+							href=""
+							class="rounded-lg px-3 py-2 font-medium text-[#dce0e6] transition duration-300 ease-in-out hover:bg-[#2b4c7e]"
+							>Testando</a
+						>
+					</div>
+				</div>
+				<div class="hidden lg:block items-center">
+					<div class="ml-10 flex items-baseline space-x-4 lg:text-xs xl:text-base">
+						<a
+							href=""
+							class="rounded-lg px-3 py-2 font-medium text-[#dce0e6] transition duration-300 ease-in-out hover:bg-[#2b4c7e]"
+							>possivel login cara</a
+						>
+					</div>
+				</div>
+		</div>
+	</div>
+</nav>
