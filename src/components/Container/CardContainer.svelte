75b65409553926a639741e11520099b3c050684a
Eduardo Silva <eduardosilvajs.py@gmail.com>
2026-04-11 10:49:42 -0400

    Lançando atualizações

---
+++ src/components/Container/CardContainer.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+</script>
+
+<section
+	class="max-w-auto m-3 grid wrap-normal sm:grid-cols-1 sm:grid-rows-1 lg:grid-cols-2 lg:grid-rows-2"
+>
+	<div class="">
+		<div
+			class="mt-10 mb-10 cursor-pointer rounded-lg bg-[#567ebb] transition duration-300 ease-in-out hover:bg-[#2b4c7e] sm:h-50 sm:w-3xl lg:h-89 lg:w-2xl"
+		>
+			vasco
+		</div>
+		<div
+			class="mt-10 mb-10 cursor-pointer rounded-lg bg-[#567ebb] transition duration-300 ease-in-out hover:bg-[#2b4c7e] sm:h-50 sm:w-3xl lg:h-89 lg:w-2xl"
+		>
+			vasco
+		</div>
+		<div
+			class="mt-10 mb-10 cursor-pointer rounded-lg bg-[#567ebb] transition duration-300 ease-in-out hover:bg-[#2b4c7e] sm:h-50 sm:w-3xl lg:h-89 lg:w-2xl"
+		>
+			vasco
+		</div>
+		<div
+			class="mt-10 mb-10 cursor-pointer rounded-lg bg-[#567ebb] transition duration-300 ease-in-out hover:bg-[#2b4c7e] sm:h-50 sm:w-3xl lg:h-89 lg:w-2xl"
+		>
+			vasco
+		</div>
+	</div>
+</section>
--- src/components/Dashboard/Dashboard.svelte
+++ src/components/Dashboard/Dashboard.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import CardContainer from '../Container/CardContainer.svelte';
 </script>
 
 <section class=" rounded-lg bg-[#dce0e6]">
@@ -6,28 +7,7 @@
 		<div class=" rounded-lg">
 			<div class="mt-5 font-medium sm:text-2xl lg:text-3xl">
 				<h1>Dashboard</h1>
-			</div>
-			<div class="flex flex-wrap justify-between sm:gap-6 sm:px-6 lg:gap-8 lg:px-8">
-				<div
-					class="mt-10 mb-10 rounded-lg bg-[#567ebb] transition duration-300 ease-in-out hover:bg-[#2b4c7e] sm:h-50 sm:w-3xl lg:h-89 lg:w-2xl"
-				>
-					vasco
-				</div>
-				<div
-					class="mt-10 mb-10 rounded-lg bg-[#567ebb] transition duration-300 ease-in-out hover:bg-[#2b4c7e] sm:h-50 sm:w-3xl lg:h-89 lg:w-2xl"
-				>
-					vasco
-				</div>
-				<div
-					class="mt-10 mb-10 rounded-lg bg-[#567ebb] transition duration-300 ease-in-out hover:bg-[#2b4c7e] sm:h-50 sm:w-3xl lg:h-89 lg:w-2xl"
-				>
-					vasco
-				</div>
-				<div
-					class="mt-10 mb-10 rounded-lg bg-[#567ebb] transition duration-300 ease-in-out hover:bg-[#2b4c7e] sm:h-50 sm:w-3xl lg:h-89 lg:w-2xl"
-				>
-					vasco
-				</div>
+				<CardContainer />
 			</div>
 		</div>
 	</div>
